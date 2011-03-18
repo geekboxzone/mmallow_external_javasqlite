93a155de666c12d2e1ba2ad99ac4d40f436a5d55
Brian Carlstrom <bdc@google.com>
2011-03-17 21:40:00 -0700

    Moving sqlite-jdbc target from libcore/JavaLibrary.mk
    Change-Id: I44955819eec75328c085b22f8912f7738d827e27

--- Android.mk
+++ Android.mk
@@ -1,13 +1,20 @@
 # -*- mode: makefile -*-
 
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-java-files-under,src/main/java)
+LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_JAVA_LIBRARIES := core
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := sqlite-jdbc
+include $(BUILD_JAVA_LIBRARY)
+
 sqlite_jdbc_src_files := \
     src/main/native/sqlite_jni.c
 sqlite_jdbc_local_c_includes := \
     $(JNI_H_INCLUDE) \
     external/sqlite/dist
-
-
-LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(sqlite_jdbc_src_files)
@@ -20,6 +27,16 @@ LOCAL_PRELINK_MODULE := false
 include $(BUILD_SHARED_LIBRARY)
 
 ifeq ($(WITH_HOST_DALVIK),true)
+    include $(CLEAR_VARS)
+    LOCAL_SRC_FILES := $(call all-java-files-under,src/main/java)
+    LOCAL_NO_STANDARD_LIBRARIES := true
+    LOCAL_JAVA_LIBRARIES := core-hostdex
+    LOCAL_JAVACFLAGS := $(local_javac_flags)
+    LOCAL_MODULE_TAGS := optional
+    LOCAL_MODULE := sqlite-jdbc-hostdex
+    LOCAL_BUILD_HOST_DEX := true
+    include $(BUILD_HOST_JAVA_LIBRARY)
+
     include $(CLEAR_VARS)
     LOCAL_SRC_FILES := $(sqlite_jdbc_src_files)
     LOCAL_C_INCLUDES += $(sqlite_jdbc_local_c_includes)
