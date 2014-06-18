b2c368c82a28686cb0d514fe234abd43a93e4270
Brian Carlstrom <bdc@google.com>
2014-06-18 15:11:01 -0700

    Switch from core to core-libart
    Bug: 14298175
    Change-Id: Ic7310d02aa06529b82a168373e9402d7da46ddb1

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under,src/main/java)
 LOCAL_NO_STANDARD_LIBRARIES := true
-LOCAL_JAVA_LIBRARIES := core
+LOCAL_JAVA_LIBRARIES := core-libart
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sqlite-jdbc
 include $(BUILD_STATIC_JAVA_LIBRARY)
